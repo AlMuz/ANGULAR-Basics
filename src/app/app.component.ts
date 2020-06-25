99495dc2be3d6e825016a9997602f716a425f56b
a.muzicenko <a.muzicenko@eco-fin.eu>
2020-06-25 16:57:10 +0300

    Event bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {  
+export class AppComponent {
   title = 'Dynamic Title';
   number = 27;
   array = [1, 2, 3];
@@ -13,10 +13,26 @@ export class AppComponent {
   img =
     'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png';
 
+  inputValue = '';
   // it must be after variable init
   constructor() {
     setTimeout(() => {
-      this.img = 'https://w7.pngwing.com/pngs/669/188/png-transparent-react-javascript-vue-js-logo-science-experiments-logo-symmetry-business.png'
-    }, 2000)
+      this.img =
+        'https://w7.pngwing.com/pngs/669/188/png-transparent-react-javascript-vue-js-logo-science-experiments-logo-symmetry-business.png';
+    }, 2000);
+  }
+
+  onInput(event: KeyboardEvent) {
+    console.log('event', event);
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onBlur(value: string) {
+    console.log(value);
+    this.inputValue = value
+  }
+
+  onClick() {
+    console.log('click');
   }
 }
